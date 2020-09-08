220ef48a8f142e464de6bc6ec4d53d326fe7825f
SiHyung92 <fjzjqhdl@gmail.com>
2020-09-08 10:52:55 +0900

    feat : 김지수 작업본 합치기, debug

---
+++ src/screens/HomeScreens.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button, AsyncStorage, Image ,ImageBackground, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+
+export default class HomeScreens extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      MenuList: [],
+      corpId: 418,
+      email: "abc@kobiznet.com",
+      password: "",
+      roles: "A",
+      userId: "",
+      userName: "admin"
+    }
+  }
+
+  // 갖고올 주소 
+  componentDidMount(){
+    const mockMenus = ["SKD 조회/견적요청","Booking 의뢰","BL 상태조회","청구확인"]; 
+    this.setState({MenuList: mockMenus});
+  }
+  
+  _retrieveData = async (key) => {
+    try {
+      const value = await AsyncStorage.getItem('key');
+      if (value !== null) {
+        return value;
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
+
+  _logOut = async () => {
+    try {
+      await AsyncStorage.removeItem(
+        'userToken'
+      );
+    } catch (error) {
+      // Error saving data
+    }
+  };
+
+  menuMove(){
+  
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('../../assets/image/main_bg.png')}  style={styles.backgroundImage}>
+          <View style={styles.Innercontainer}>
+            <View style={styles.Middle}>
+              <Image
+                  style={styles.MAN_IMG} 
+                  source={require('../../assets/image/img_user.png')}/>
+              <Text
+                  style={styles.CORP_NM}
+              >(주) 한국비즈넷</Text>
+            </View>  
+            <View style={styles.Down}>  
+              {this.state.MenuList.map((value) =>
+              <Text
+                style={styles.MenuBtn}
+                key={value}
+                onClick ={()=>this.menuMove() }>
+                {value}
+              </Text>)
+              } 
+            </View>
+            <View>
+              <TouchableOpacity 
+                style={styles.actionButton}
+                onPress={() => this.navigation.dispatch(DrawerActions.openDrawer())}>          
+                <Text>으아 똥</Text>
+              </TouchableOpacity>
+           </View>
+          </View>
+        </ImageBackground>
+      </View>
+     );
+    }
+  }
+
+const styles = StyleSheet.create({
+  container : {
+     flex: 1,
+     alignItems: 'center',
+     justifyContent: 'center',
+     flexDirection: 'column', 
+  },
+  backgroundImage :{
+    width: "100%",
+    height : "100%"
+  },
+  Innercontainer:{
+    paddingTop : '5%'
+  },
+
+  Middle:{
+    flex: 2,
+    alignItems:"center"
+    ,justifyContent:"center",
+    marginTop : 10
+  },
+  MAN_IMG :{
+    width: 60,
+    height: 60,
+  },
+  CORP_NM:{
+    fontSize: 24,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 36,
+    letterSpacing: -1.2,
+    textAlign: "left",
+    color: "#ffffff", 
+  },
+
+  Down:{
+    flex: 3,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40
+  },
+  MenuBtn:{
+    fontSize: 18,
+    lineHeight: 27,
+    letterSpacing: -0.9,
+    
+    color: "#ffffff",
+    width: 312,
+    height: 51,
+    borderRadius: 5,
+    backgroundColor: "#2467b8",
+    shadowColor: "rgba(0, 0, 0, 0.16)",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+    textAlign :"center",
+    textAlignVertical :"center",
+    lineHeight :51,
+    marginBottom : 20
+
+  },
+  actionButton: {
+    flex:1,
+    alignSelf : 'flex-end',
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    
+  },
+})
--- src/screens/MainScreens.js
+++ src/screens/MainScreens.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image ,ImageBackground  } from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import { createDrawerNavigator,  DrawerContentScrollView,  DrawerItemList,  DrawerItem,drawerPosition} from '@react-navigation/drawer';
+import { createDrawerNavigator,  DrawerContentScrollView,  DrawerItemList,  DrawerItem } from '@react-navigation/drawer';
+import HomeScreens from './HomeScreens';
 
 function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button
         title="Open drawer"
-        
         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
       />
     </View>
@@ -26,14 +26,79 @@ function Feed({ navigation }) {
 */
 function CustomDrawerContent(props) {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
-      />
 
-    </DrawerContentScrollView>
+    <View style={styles.sideBarOut}>
+      <ImageBackground
+          source={require('../../assets/image/toggle_bg.png')}  style={styles.backgroundImage}>
+      <View style={styles.UP}>
+            <View  style={styles.Vertical}>
+                <View style={styles.WIDE1}>
+                  <Image
+                    style={styles.H_L_IMG} 
+                    source={require('../../assets/image/home.png')}/>
+                  <Text  style={styles.H_L}>HOME</Text>
+                  <Text  style={styles.H_L}>  |  </Text>
+                  <Image
+                    style={styles.H_L_IMG} 
+                    source={require('../../assets/image/logout.png')}/>
+                  <Text  style={styles.H_L}>LOGOUT</Text>
+                </View>
+                <View style={styles.WIDE2}>
+                <Image
+                  style={{ width: 40,
+                          height: 40}}
+                  source={require('../../assets/image/img_muser.png')}/>
+                <Text  style={styles.C}>
+                  회사이름</Text>
+                <Image 
+                  style={styles.modify_IMG}
+                  source={require('../../assets/image/edit_ico.png')}/>
+                </View>
+
+                <View style={styles.WIDE3}>
+                <View style={styles.round}>
+                  <Image
+                    style={styles.S_O_IMG} 
+                    source={require('../../assets/image/ic_speaker.png')}/>
+
+                   <Text  style={styles.S_O}>
+                        고객센터</Text>
+                </View>
+                <View style={styles.round}>
+                <Image
+                    style={styles.S_O_IMG} 
+                    source={require('../../assets/image/ic_settings.png')}/>
+                    <Text  style={styles.S_O}>
+                        설정</Text>
+                </View>
+                </View>
+
+                <View style={styles.WIDE4}>
+                <View style={styles.cancelround}>
+                  <Image
+                      style={{ width: 17,
+                              height: 17,
+                              marginLeft : 15,
+                              marginTop : 15
+                            }}
+                      source={require('../../assets/image/ic_close.png')}/>
+                </View>
+                </View>
+            </View>  
+               
+        </View>
+        </ImageBackground>
+
+      <View style={styles.Down}>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close drawer"
+          onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
+        />
+      </DrawerContentScrollView>
+      </View>
+    </View>
   );
 }
 ////////////////////////////////////////////
@@ -44,19 +109,129 @@ const Drawer = createDrawerNavigator(
 //사이드바 메뉴 항목 추가 .
 function MyDrawer() {
   return (
-    <Drawer.Navigator drawerPosition ='left'  drawerContent={props => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Feed" component={Feed} />
-     
-      
-    </Drawer.Navigator>
-    
+    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="HomeScreens" component={HomeScreens} />
+    </Drawer.Navigator> 
   );
 }
 /////////////////////
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyDrawer />
-    </NavigationContainer>
-  );
+export default class MainScreens extends React.Component{
+  static navigationOptions = {
+      title: "main",
+      headerShown: null,
+  }
+  render(){
+    return (
+      <NavigationContainer>
+        <MyDrawer />
+      </NavigationContainer>
+    )
+  }
+
 }
+const styles = StyleSheet.create({
+  sideBarOut :{
+    flex: 1
+  },
+  UP : {
+    flex: 1  ,
+    justifyContent: 'center',
+    marginLeft : 30
+  },
+  Down:{
+    flex: 2.5  
+  },
+  
+  WIDE1:{
+    flexDirection: 'row',
+    //alignitems: 'flex-start',
+    marginTop : 15,
+  },
+  WIDE2:{
+    flexDirection: 'row',
+    //alignitems: 'flex-start',
+    marginTop : 35,
+  },
+  WIDE3:{
+    flexDirection: 'row',
+    //alignitems: 'flex-start',
+    marginTop : 35,
+  },
+  WIDE4:{
+    flexDirection: 'row'
+    ,    marginLeft :235,
+    marginTop : -40
+  },
+  H_L_IMG :{
+    marginTop :3 ,
+    width: 12.2,
+    height: 12.2,
+  },
+  S_O_IMG:{
+    marginTop :10 ,
+    width: 12.2,
+    height: 12.2,
+  },
+  modify_IMG:{
+    marginTop :17,
+    width: 12.2,
+    height: 12.2,
+  },
+  round:{
+    width: 82,
+    height: 36,
+    borderRadius: 18,
+    backgroundColor: "#002254",
+    flexDirection: 'row',
+
+  },
+  cancelround :{
+    width: 50,
+    height: 50,
+    backgroundColor: "#367fff",
+    borderRadius: 100,
+    marginTop : 10
+  },
+  backgroundImage :{
+    flex: 1,
+  },
+  H_L :{
+
+    //fontFamily: "NotoSansKR",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: 20,
+    letterSpacing: -0.7,
+    textAlign: "left",
+    color: "#ffffff"
+
+  },
+  C:{
+    width: 113,
+    height: 28,
+    //fontFamily: "NotoSansKR",
+    fontSize: 19,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 28,
+    letterSpacing: -0.95,
+    textAlign: "left",
+    color: "#ffffff"
+    ,marginTop : 10
+    , marginLeft : 10
+  },
+  S_O:{
+
+   // fontFamily: "NotoSansKR",
+    fontSize: 14,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: 20,
+    letterSpacing: -0.7,
+    textAlign: "left",
+    color: "#ffffff"
+    ,marginTop : 8
+    ,marginLeft : 5
+  }
+})
